5448a644dbddbe5e8ff46f41617fa25e44c7c2a8
Logan Bussell <lsbussell@gmail.com>
2018-09-14 10:13:22 -0400

    added audioread, mono downsample, and filter

--- assignment2/ass2main.m
+++ assignment2/ass2main.m
@@ -1,7 +1,14 @@
-
-[audio, sampleRate] = audioread('serato_bigband.wav');
 a = [1,-4.019576181115830,6.189406442920687,-4.453198903544109,1.420842949621874,-0.141825473830304,0.004351177233495];
 b = [0.255741125204258,-0.511482250408518,-0.255741125204251,1.022964500817026,-0.255741125204258,-0.511482250408514,0.255741125204258];
+
+[xs, fs] = audioread('serato_bigband.wav');
+x = sum(xs, 2) / size(xs, 2);
+x_a = filter(b,a,x);
+
+rms_x = myRms(x);
+rms_x_a = myRms(x_a);
+
+% sound(x_a, fs);
 
 % 6. [15] Add calls to the above functions to your main function.
 % Then, add plot commands to display
