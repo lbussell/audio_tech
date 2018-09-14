004f974d385ed5ccb41681dd94ef11b16f628bd8
Logan Bussell <lsbussell@gmail.com>
2018-09-14 10:12:04 -0400

    implemented myRms function

--- assignment2/myRms.m
+++ assignment2/myRms.m
@@ -1,11 +1,6 @@
 function [res] = myRms(samplevector)
 
-% 2. [10] Write a function [res] = myRms(samplevector) that returns the
-  % root mean square (one value) of the signal block given in samplevector.
-  
-% Describe how you verified the correctness of your implementation.
-
-res = samplevector;
+% Returns the root mean square of the signal block given in samplevector.
+res = sqrt(sum(samplevector.^2)/length(samplevector));
 
 end
-
